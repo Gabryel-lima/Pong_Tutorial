d8993a3e148897fb4425fab645bd59d30e07eb2a
Gabryel-lima <gabbryellimasi@gmail.com>
2025-05-06 19:21:08 -0300

    Add requirements and setup script for Pong game project

---
+++ setup.py
@@ -0,0 +1,27 @@
+import sys
+from cx_Freeze import setup, Executable
+
+if sys.platform == 'win32':
+    base = 'Win32GUI'  # Para Windows, use Win32GUI para não mostrar o console
+else:
+    base = None
+
+build_options = {
+    'packages': ['pygame-ce'],
+    'include_files': ['sounds/', 'data/', 'src/']
+}
+
+executables = [
+        Executable(
+            'src/Pong.py', base=base, 
+            target_name='Pong', icon='./assets/icon.png'
+    )
+]
+
+setup(
+    name='Pong_Tutorial',
+    version='1.0',
+    description='Este projeto é um tutorial de como criar um jogo de Pong utilizando Python e Pygame. Além disso, ele inclui um agente de aprendizado por reforço que joga o jogo utilizando um algoritmo evolutivo.',
+    options={'build_exe': build_options},
+    executables=executables
+)
